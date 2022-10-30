44e96322c96d551d3d952cabc40293cc90f594be
laxcode <89154828+laxcode@users.noreply.github.com>
2022-10-30 22:41:02 +0530

    Delete stringPro.js

--- stringPro.js
+++
@@ -1,8 +0,0 @@
-function getMyLength(stringValue){
-    console.log(document.getElementById('inputValue').value.length);
-    document.getElementById('lengthNum').innerHTML=stringValue.length;
-}
-function sliceForMe(stringValue,startIndex,endIndex){
-    
-    document.getElementById('sliceForme').innerHTML=stringValue.slice(startIndex,endIndex);
-}
